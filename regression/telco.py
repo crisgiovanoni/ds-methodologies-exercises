ac5bf3bbcede6bbda1c6fc4bfd6212b1b896df58
Cris Giovanoni <giovanonicris@gmail.com>
2019-10-21 16:33:19 -0500

    Inital Commit

--- regression/telco.py
+++ regression/telco.py
@@ -9,7 +9,7 @@ import features
 ### SelectKBest - Top Features of Unscaled Data
 
 ## Step 1. Load Data
-telco_df = wrangle.wrangle_telco()
+telco_df = wrangle_telco()
 telco_df.head()
 telco_X = telco_df[["monthly_charges","tenure"]]
 telco_y = telco_df["total_charges"]
@@ -18,4 +18,4 @@ telco_y = telco_df["total_charges"]
 telco_X_train, telco_X_test, telco_y_train, telco_y_test = split_my_data(telco_X,telco_y, 0.80)
 
 ## Step 3. Run select_kbest_freg_unscaled
-features.select_kbest_freg_unscaled(telco_X_train,telco_y_train,1)
+f_features = features.selectkbest_optimal_features(telco_X_train,telco_y_train,2)
