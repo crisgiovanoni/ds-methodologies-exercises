80ab1c4199d237e0add17e5b19af80e18695132f
Cris Giovanoni <giovanonicris@gmail.com>
2019-12-05 11:30:49 -0600

    finished all codeup article functions

--- nlp/acquire.py
+++ nlp/acquire.py
@@ -2,28 +2,6 @@ from requests import get
 from bs4 import BeautifulSoup
 import os
 
-#Starters
-url = "https://codeup.com/codeups-data-science-career-accelerator-is-here/"
-url = "https://codeup.com/data-science-myths/"
-url = "https://codeup.com/data-science-vs-data-analytics-whats-the-difference/"
-headers = {'User-Agent': 'Bayes DS'}
-response = get(url, headers=headers)
-
-#Sanity check if we got the right website
-print(response.text)
-
-soup = BeautifulSoup(response.content, 'html.parser')
-title = soup.select('#mk-page-introduce > div > h1')
-title = title[0].get_text()
-
-title
-
-article = soup.find('div',class_="mk-single-content")
-article = article.text
-
-title
-article
-
 def make_dictionary_from_article(url):
     #Connect to internet
     url_path = url
@@ -33,8 +11,8 @@ def make_dictionary_from_article(url):
     #Soup the title and body of article
     soup = BeautifulSoup(response.content, 'html.parser')
     
-    title = soup.select('#mk-page-introduce > div > h1')
-    title = title[0].get_text()
+    #get title through selector or html title element 
+    title = soup.title
 
     body = soup.find('div',class_="mk-single-content")
     body = body.text
@@ -43,28 +21,7 @@ def make_dictionary_from_article(url):
         "title": title,
         "body": body
     }
-    
-    
 
-# https://codeup.com/codeups-data-science-career-accelerator-is-here/
-# https://codeup.com/data-science-myths/
-# https://codeup.com/data-science-vs-data-analytics-whats-the-difference/
-# https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/
-# https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/
-
-
-def make_dictionary_from_article(url):
-    # make the request to the url variable
-    # make a "soup" variable
-    # isolate the title of the article, store it as a string called "title"
-    # isolate the body text of the article (buy CSS selector), name the variable "body"
-    
-    return {
-        "title": title,
-        "body": body
-    }
-​
-​
 def get_blog_articles():
     urls = [
         "https://codeup.com/codeups-data-science-career-accelerator-is-here/",
@@ -73,10 +30,9 @@ def get_blog_articles():
         "https://codeup.com/10-tips-to-crush-it-at-the-sa-tech-job-fair/",
         "https://codeup.com/competitor-bootcamps-are-closing-is-the-model-in-danger/",
     ]
-​
-    output = []
-    
+
+    full_articles = []
     for url in urls:
-        output.append(make_dictionary_from_article(url))
-​
-    return output
+        full_articles.append(make_dictionary_from_article(url)) 
+
+    return full_articles
